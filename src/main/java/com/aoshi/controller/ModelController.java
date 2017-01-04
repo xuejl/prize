3b6ef7c2932b0ea7cbc7cc59da9e5a54b195b940
Alison <Alison>
2017-01-04 14:12:11 +0800

    update

--- src/main/java/com/aoshi/controller/ModelController.java
+++ src/main/java/com/aoshi/controller/ModelController.java
@@ -1,6 +1,7 @@
 package com.aoshi.controller;
 
 import Decoder.CharacterEncoder;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -12,7 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -29,13 +32,32 @@ public class ModelController {
     Logger logger = LoggerFactory.getLogger(ModelController.class);
 
     private static final String ZIP_FILE_NAME = "model.zip";
+//    private static final String oldPath = "D:\\xjl_code\\Prize\\src\\main\\webapp\\model";
+//    private static final String newPath = "D:\\xjl_code\\Prize\\src\\main\\webapp\\file";
+    private static final String path = "D:\\xjl_code\\Prize\\src\\main\\webapp\\new\\";
+
     @RequestMapping("download")
     public String downLoadFile(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
         File file = getModelFile(session);
         List<String> contents = getDataFromDataBase();
         String fileContent = getModelFileToString(file);
         fileContent = replaceModelTag(fileContent, contents);
-        GenZipFileInLocal(session, fileContent);
+        //生成模板新的html
+        byte tag_bytes[] = fileContent.getBytes();
+        // 根据"new"+模板名得到文件名。
+        String fileame = "new"+ file.getName().substring(0,file.getName().indexOf(".")) + ".html";
+        // 生成的html文件保存路径。
+        fileame = session.getServletContext().getRealPath("/") + File.separator + "new" +"/"+ fileame;
+        FileOutputStream fileoutputstream = new FileOutputStream(fileame);// 建立文件输出流
+        fileoutputstream.write(tag_bytes);
+        fileoutputstream.close();
+        //复制文件
+//        copyFolder(path,newPath);
+        //GenZipFileInLocal(session, fileContent);
+
+        //压缩zip文件
+        fileToZip(session.getServletContext().getRealPath("/") + File.separator + "new" ,
+                session.getServletContext().getRealPath("/") + File.separator + "file", ZIP_FILE_NAME);
         try {
             getClientFile(response, ZIP_FILE_NAME, session);
         } catch (FileNotFoundException e) {
@@ -56,7 +78,7 @@ public class ModelController {
      * @param fileContent
      * @param contents
      */
-    private String replaceModelTag(String fileContent, List<String> contents) {
+    private String replaceModelTag(String fileContent, List<String> contents) throws IOException {
         for (int i = 0; i < contents.size(); i++) {
             fileContent = fileContent.replace(String.format("[%s]", String.valueOf(i+1)), contents.get(i));
         }
@@ -131,6 +153,9 @@ public class ModelController {
             add("android移动开发");
             add("IOS移动开发");
             add("Html5前端开发");
+            String url = "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png";
+            String img = downloadFromUrl(url, path);
+            add(img);
         }};
     }
 
@@ -142,5 +167,132 @@ public class ModelController {
         String path = session.getServletContext().getRealPath("/") + File.separator;
         logger.debug("app", "path == > " + path);
         return new File(path + "model" + File.separator + "modelA.html");
+    }
+
+    /**
+     * 复制文件
+     */
+    public void copyFolder(String oldPath, String newPath) {
+
+        try {
+            (new File(newPath)).mkdirs(); //如果文件夹不存在 则建立新文件夹
+            File a=new File(oldPath);
+            String[] file=a.list();
+            File temp=null;
+            for (int i = 0; i < file.length; i++) {
+                if(oldPath.endsWith(File.separator)){
+                    temp=new File(oldPath+file[i]);
+                }
+                else{
+                    temp=new File(oldPath+File.separator+file[i]);
+                }
+
+                if(temp.isFile()){
+                    FileInputStream input = new FileInputStream(temp);
+                    FileOutputStream output = new FileOutputStream(newPath + "/" +
+                            (temp.getName()).toString());
+                    byte[] b = new byte[1024 * 5];
+                    int len;
+                    while ( (len = input.read(b)) != -1) {
+                        output.write(b, 0, len);
+                    }
+                    output.flush();
+                    output.close();
+                    input.close();
+                }
+                if(temp.isDirectory()){//如果是子文件夹
+                    copyFolder(oldPath+"/"+file[i],newPath+"/"+file[i]);
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("复制整个文件夹内容操作出错");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static boolean fileToZip(String sourceFilePath,String zipFilePath,String fileName){
+        boolean flag = false;
+        File sourceFile = new File(sourceFilePath);
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        FileOutputStream fos = null;
+        ZipOutputStream zos = null;
+
+        if(sourceFile.exists() == false){
+            System.out.println("待压缩的文件目录："+sourceFilePath+"不存在.");
+        }else{
+            try {
+                File zipFile = new File(zipFilePath + "/" + fileName);
+                if(zipFile.exists()){
+                    System.out.println(zipFilePath + "目录下存在名字为:" + fileName +"打包文件.");
+                }else{
+                    File[] sourceFiles = sourceFile.listFiles();
+                    if(null == sourceFiles || sourceFiles.length<1){
+                        System.out.println("待压缩的文件目录：" + sourceFilePath + "里面不存在文件，无需压缩.");
+                    }else{
+                        fos = new FileOutputStream(zipFile);
+                        zos = new ZipOutputStream(new BufferedOutputStream(fos));
+                        byte[] bufs = new byte[1024*10];
+                        for(int i=0;i<sourceFiles.length;i++){
+                            //创建ZIP实体，并添加进压缩包
+                            ZipEntry zipEntry = new ZipEntry(sourceFiles[i].getName());
+                            zos.putNextEntry(zipEntry);
+                            //读取待压缩的文件并写进压缩包里
+                            fis = new FileInputStream(sourceFiles[i]);
+                            bis = new BufferedInputStream(fis, 1024*10);
+                            int read = 0;
+                            while((read=bis.read(bufs, 0, 1024*10)) != -1){
+                                zos.write(bufs,0,read);
+                            }
+                        }
+                        flag = true;
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } finally{
+                //关闭流
+                try {
+                    if(null != bis) bis.close();
+                    if(null != zos) zos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return flag;
+    }
+
+    private String downloadFromUrl(String url,String dir) {
+        String fileName = "";
+        try {
+            URL httpurl = new URL(url);
+             fileName = getFileNameFromUrl(url);
+            File f = new File(dir + fileName);
+            FileUtils.copyURLToFile(httpurl, f);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileName;
+    }
+
+    private String getFileNameFromUrl(String url){
+        String name = new Long(System.currentTimeMillis()).toString() + ".X";
+        int index = url.lastIndexOf("/");
+        if(index > 0){
+            name = url.substring(index + 1);
+            if(name.trim().length()>0){
+                return name;
+            }
+        }
+        return name;
     }
 }
